b5b42caa93f67e5f33414f8cf1972fe76d9cc13f
bharath210 <barathkumargoudpalsa@gmail.com>
2020-08-18 19:56:47 +0530

    Appium

---
+++ AppiumWithJavaLearning/src/test/java/com/calculator/AppiumWithJavaLearning/CalculatorTest.java
@@ -0,0 +1,50 @@
+package com.calculator.AppiumWithJavaLearning;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+
+public class CalculatorTest
+{
+	public AndroidDriver<MobileElement> driver;
+	@BeforeTest
+	public void launchApp() throws MalformedURLException
+	{
+		DesiredCapabilities cap = new DesiredCapabilities();
+		cap.setCapability("deviceName", "Galaxy On5");
+		cap.setCapability("udid", "420053709b9133a5");
+		cap.setCapability("platformName", "Android");
+		cap.setCapability("platformVersion", "6.0.1");
+	
+		cap.setCapability("appPackage", "com.sec.android.app.popupcalculator");
+		cap.setCapability("appActivity", "com.sec.android.app.popupcalculator.Calculator");
+		
+		URL url = new URL("http://127.0.0.1:4723/wd/hub");
+		driver = new AndroidDriver<MobileElement>(url,cap);
+	}
+	@AfterTest
+	public void closeApp()
+	{
+		driver.close();
+	}
+	@Test
+	public void test_add()
+	{
+		driver.findElement(By.id("com.sec.android.app.popupcalculator:id/bt_02")).click();
+		driver.findElement(By.id("com.sec.android.app.popupcalculator:id/bt_add")).click();
+		driver.findElement(By.id("com.sec.android.app.popupcalculator:id/bt_03")).click();
+		driver.findElement(By.id("com.sec.android.app.popupcalculator:id/bt_equal")).click();
+		String result = driver.findElement(By.id("com.sec.android.app.popupcalculator:id/txtCalc")).getText();
+		System.out.println(result);
+	}
+}
